12849b244bec30c50909d46120e7509351aed6e3
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2022-07-04 12:08:01 +0700

    Update GitHubApiService.kt

--- app/src/main/java/com/nauval/aplikasigithubuser/helper/GitHubApiService.kt
+++ app/src/main/java/com/nauval/aplikasigithubuser/helper/GitHubApiService.kt
@@ -8,22 +8,17 @@ import retrofit2.http.Query
 
 interface GitHubApiService {
     @GET("users")
-//    @Headers("Authorization: token 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")
     fun getUsers(@Query("page") page: String): Call<List<UserResponse>>
 
     @GET("search/users")
-//    @Headers("Authorization: token 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")
     fun findUser(@Query("q") username: String): Call<UserSearchResponse>
 
     @GET("users/{username}")
-//    @Headers("Authorization: token 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")
     fun getUserDetail(@Path("username") username: String): Call<UserDetailsResponse>
 
     @GET("users/{username}/followers")
-//    @Headers("Authorization: token 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")
     fun getFollowers(@Path("username") username: String): Call<List<UserResponse>>
 
     @GET("users/{username}/following")
-//    @Headers("Authorization: token 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")
     fun getFollowings(@Path("username") username: String): Call<List<UserResponse>>
 }
